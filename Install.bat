eb2231205b8524c776956dc026bdd70d8e88e7bd
lyall-pc <lyall.yeet@gmail.com>
2022-05-31 20:01:31 +0100

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,2 +1,2 @@
 @echo off
-npm i chalk@4.1.2 fluent-ffmpeg@2.1.2 prompts@2.4.2 youtube-sr@4.1.17 ytdl-core@4.11.0
+npm i chalk@4.1.2 fluent-ffmpeg@2.1.2 prompts@2.4.2 youtube-sr@4.1.17 ytdl-core@4.11.0 node-fetch@2
